19defe66b08daa14023efc8029104fe082067c9a
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2023-12-19 21:21:54 +0700

    feat: init app

---
+++ android/app/src/main/kotlin/com/nielio/daresto/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nielio.daresto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/pages.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+part 'page_home.dart';
