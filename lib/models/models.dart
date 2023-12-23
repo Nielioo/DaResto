2442396e8a84535b693bf3c5078eeb95fdb396a6
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2023-12-23 10:10:05 +0700

    feat: create models

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -6,4 +6,8 @@ part 'restaurant_list_model.dart';
 part 'get_restaurant_detail_model.dart';
 part 'restaurant_detail_model.dart';
 part 'restaurant_detail_category_model.dart';
-part 'restaurant_detail_customer_review_model.dart';
+
+part 'restaurant_search_model.dart';
+
+part 'get_customer_review_model.dart';
+part 'customer_review_model.dart';
